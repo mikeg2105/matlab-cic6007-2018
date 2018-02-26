ade535308e1937644b8261367e7e72a8e81c087d
mikeg64 <m.griffiths@sheffield.ac.uk>
2018-02-26 20:28:14 +0000

    Updated getting faster with matlab

--- matlab_examples/numericalanalysis/interpol/interpol.m
+++ matlab_examples/numericalanalysis/interpol/interpol.m
@@ -1,47 +1,39 @@
-%Integration using Simpsons rule
-
-%quadrature to higher order using lagrange interpolation
-
-n=10;  %size of interval
+%Using lagrange interpolation polynomial
+n=20;  %size of interval
 
 %use an odd number of elements
 n=(n*2)+1;
 
-%integration interval
-a=0;
-b=pi/2;
+%interval for polynomial
+a=-4;
+b=3;
+
+%interval for sine function
+%a=0;
+%b=pi/2;
 
 h=(b-a)/n;
-
 x=a:h:b;
 
+%try modifying the test function
 for i=1:n+1
     y(i)=testfunction(x(i));
 end
 
-sum=0;
+%test at one point, is this correct?
+f=y;
+xval=1.43;
+ii=20;
+val=lagrange_interp(xval,f,x,ii);
 
-%since we are using interpolation
-%the loop performs the summation 
-%over every other element
-for i=2:2:n-1
-    sum=sum+y(i-1)+4*y(i)+y(i+1);
+xint=a+(3*h/2):h:b-(3*h/2);
+for i=1:numel(xint)
+    yint(i)=lagrange_interp(xint(i),y,x,i+2);   
 end
 
-result=sum*h/3;
-
-disp('results is: ');
-disp(result);
-
-xval=1.43;
-f=y;
-ii=20;
-
-val=lagrange_interp(xval,f,x,ii)
-
-
-
-
+plot(x,y);
+hold on;
+plot(xint,yint,'s');
 %lagrange interpolation
 
 
--- matlab_examples/numericalanalysis/interpol/lagrange_interp.m
+++ matlab_examples/numericalanalysis/interpol/lagrange_interp.m
@@ -1,19 +1,13 @@
-%xval is the value of x at which we wish to interpolate
-%f is the series of values
-function y=lagrange_interp(xval,f,x,i)  
+function y=lagrange_interp(xval,y,x,i) 
+%% langrange_interp - Using a series of points use an interpolating polynomial to
+%  evaluate att a point xval
+%  xval is the value of x at which we wish to interpolate
+%  x and y are the series of points x with the value y at each of those
+%  points
+%%
+
     t1=(xval-x(i))*(xval-x(i+1))/((x(i-1)-x(i))*(x(i-1)-x(i+1)));
     t2=(xval-x(i-1))*(xval-x(i+1))/((x(i)-x(i-1))*(x(i)-x(i+1)));
     t3=(xval-x(i-1))*(xval-x(i))/((x(i+1)-x(i-1))*(x(i+1)-x(i)));
-    y=t1*f(i-1)+t2*f(i)+t3*f(i+1);
+    y=t1*y(i-1)+t2*y(i)+t3*y(i+1);
 
-
-%Here is the correct working version
-
-
-          % t1=(xval-x(i+1))/(x(i)-x(i-1));
-          % t2=(xval-x(i))/(x(i+1)-x(i));
-          % y=t1*f(i)+t2*f(i+1);   
-
-
-
-%endfunction
